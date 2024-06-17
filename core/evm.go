237351aa56db53fc66df6dbc630f15a2a86b937e
Jack Li <jack@artela.network>
2024-06-17 17:46:52 +0800

    feat: add gas measurement for aspect core contract

--- core/evm.go
+++ core/evm.go
@@ -123,11 +123,6 @@ func GetHashFn(ref *types.Header, chain ChainContext) func(n uint64) common.Hash
 // CanTransfer checks whether there are enough funds in the address' account to make a transfer.
 // This does not take the necessary gas in to account to make the transfer valid.
 func CanTransfer(db vm.StateDB, addr common.Address, amount *big.Int) bool {
-	if addr.String() == "0x0000000000000000000000000000000000A27E14" {
-		// FIXME: skip fee charging for any aspect related operations,
-		//        will fix this after the proper gas instruments gets implemented in artwasm
-		return true
-	}
 	return db.GetBalance(addr).Cmp(amount) >= 0
 }
 
